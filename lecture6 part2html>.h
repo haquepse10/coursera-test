d6513eafa4ff4c1639547a4f72157fb90262c51b
haquepse10 <92968298+haquepse10@users.noreply.github.com>
2021-11-26 11:38:36 +0530

    Create lecture6 part2html>.h

---
+++ lecture6 part2html>.h
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  <title>Heading elements</title>
+  </head>
+  <body>
+ <header>
+   header is a element-Some header information goes here .Usually consists of company logo,some tag lines,etc.Some,navigation is contained in the header as well.
+   <nav>nav(shortof navigation)element-Usually contains links to different parts of the website</nav>
+ </header>
+    <h1>
+      Main Headin of this page ( hard not to have it )
+    </h1>
+    <section>
+      Section1
+      <article>Article 1
+      </article>
+      <article>Article 2</article>
+      <article>Article 3</article>
+    </section>
+    <section>
+      Section2
+      <article>Article 4</article>
+      <article>Article 5</article>
+      <article>Article 6</article>
+      <div>
+       Regular DIV element 
+      </div>
+    </section>
+    <aside>
+      ASIDE= Some information that relates to main topic,i.e.,
+      related post.
+    </aside>
+    
+    
+    <footer>
+      ABDUL HAQ copyright2021
+    </footer>
+  </body>
+  
+</html>
